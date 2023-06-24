b0f19f2002d1f8ecfb2cdbbe6b452a552796b1d9
VipulVW14 <vipulwasnik0@gmail.com>
2023-06-24 15:52:03 +0530

    pusing kirats soln for fileServer.js

--- Week2/Assignment/fileServer.js
+++ Week2/Assignment/fileServer.js
@@ -4,40 +4,37 @@ const app = express();
 const port = 3000;
 const fs= require('fs');
 const path= require('path');
-const folderPath= './files';
+
 app.get('/', (req, res) => {
   res.send('File Server is running!')
 });
 
-app.get('/files', (req,res)=>{
-    fs.readdir(folderPath, (err,files)=>{
-        if(err){
-            console.error('Error reading folder: ',err);
-            return;
-        } 
-        res.status(200).json(files);
-    })
-})
-
-app.get('/files/:filename', (req,res)=>{
-    const filename = req.params.filename;
-    console.log(filename);
-
-    const filePath=`./files/${filename}`;
-    const fileExists= fs.existsSync(filePath);
-    if(fileExists){
-        fs.readFile(`./files/${filename}`,"utf-8", (err, data)=>{console.log(data);})
+app.get('/files', (req, res) => {
+  fs.readdir(path.join(__dirname, './files/'), (err, files) => {
+    if (err) {
+      return res.status(500).json({ error: 'Failed to retrieve files' });
     }
-    else console.log('File not found!');
-})
-
+    res.json(files);
+  });
+});
+  
+app.get('/file/:filename', (req, res) => {
+  const filepath = path.join(__dirname, './files/', req.params.filename);
+  
+  fs.readFile(filepath, 'utf8', (err, data) => {
+    if (err) {
+      return res.status(404).send('File not found');
+    }
+    res.send(data);
+  });
+});
+  
+app.all('*', (req, res) => {
+  res.status(404).send('Route not found');
+});
+  
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
 module.exports = app;
 
-// // Creating http server using Node.js using fs library for fetching data of a file//
-// function callbackFn(err, data){
-//     console.log(data);
-// }
-// fs.readFile("a.txt","utf-8",callbackFn)
